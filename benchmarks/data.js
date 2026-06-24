1543f02c3168dce07c08108fc548f67da757dfb4
github-action-benchmark <github@users.noreply.github.com>
2026-06-24 10:24:52 +0000

    add paradedb_wikipedia_concurrent Benchmarks (customSmallerIsBetter) benchmark result for 578ff529894992fb9e67cae4c69424e65c84868e

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782296684981,
+  "lastUpdate": 1782296692768,
   "repoUrl": "https://github.com/nagyist/pg_textsearch",
   "entries": {
     "cranfield Benchmarks": [
@@ -28197,6 +28197,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "paradedb_wikipedia_concurrent - Concurrent Insert Time",
             "value": 14322.353968,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Todd J. Green",
+            "username": "tjgreen42",
+            "email": "tjgreen@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "578ff529894992fb9e67cae4c69424e65c84868e",
+          "message": "Release v1.3.1 (#419)\n\n## Summary\n- prepare pg_textsearch v1.3.1 from current main\n- rename release SQL artifacts and set default/library version to 1.3.1\n- rename the release banner reference and image to v1.3.1\n\n## Release procedure\n- audited v1.3.0 -> current SQL delta; only new catalog object is\nbm25_pending_free_pages(text), covered by\nsql/pg_textsearch--1.3.0--1.3.1.sql\n- left upgrade-tests old_version matrix unchanged because it already\nincludes previous release 1.3.0\n\n## Validation\n- PG_CONFIG=/home/azureuser/pg17/bin/pg_config make\n- PG_CONFIG=/home/azureuser/pg17/bin/pg_config make format-check\n- PATH=/home/azureuser/pg17/bin:$PATH\nPG_CONFIG=/home/azureuser/pg17/bin/pg_config PGHOST=/tmp make test-local",
+          "timestamp": "2026-06-23T01:39:54Z",
+          "url": "https://github.com/nagyist/pg_textsearch/commit/578ff529894992fb9e67cae4c69424e65c84868e"
+        },
+        "date": 1782296692082,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "paradedb_wikipedia_concurrent - Index Build Time",
+            "value": 5.97,
+            "unit": "ms"
+          },
+          {
+            "name": "paradedb_wikipedia_concurrent - Insert Time",
+            "value": 43.201,
+            "unit": "ms"
+          },
+          {
+            "name": "paradedb_wikipedia_concurrent - Concurrent Insert Time",
+            "value": 13894.893263,
             "unit": "ms"
           }
         ]
